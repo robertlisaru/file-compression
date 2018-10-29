b2a837446424e2e56bf08a44721503e76edcb369
Lisaru Robert <lisaru.robert@hotmail.com>
2018-10-29 23:17:03 +0200

    Create a read after write test

---
+++ src/test/java/ro/ulbsibiu/ccsd/laboratory/robert/test/bitio/ReadWriteTest.java
@@ -0,0 +1,35 @@
+package ro.ulbsibiu.ccsd.laboratory.robert.test.bitio;
+
+import org.junit.jupiter.api.Test;
+import ro.ulbsibiu.ccsd.laboratory.robert.bitio.BitReader;
+import ro.ulbsibiu.ccsd.laboratory.robert.bitio.BitWriter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReadWriteTest {
+    BitReader bitReader;
+    BitWriter bitWriter;
+    ByteArrayOutputStream outputStream;
+    InputStream inputStream;
+
+    @Test
+    void readAfterWrite() throws IOException {
+        outputStream = new ByteArrayOutputStream();
+        bitWriter = new BitWriter(outputStream);
+        bitWriter.writeNBitValue(10, 6);
+        bitWriter.writeBit(1);
+        bitWriter.writeNBitValue(11, 6);
+        bitWriter.flush();
+
+        inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        bitReader = new BitReader(inputStream);
+        assertEquals(10, bitReader.readNBitValue(6));
+        assertEquals(1, bitReader.readBitAsInt());
+        assertEquals(11, bitReader.readNBitValue(6));
+    }
+}
