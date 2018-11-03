c55b894639eefb31aeead813c5bc493c8cdd8fbd
Lisaru Robert <lisaru.robert@hotmail.com>
2018-11-03 03:48:45 +0200

    Test compute dictionary, encode, write, flush

--- src/test/java/ro/ulbsibiu/ccsd/laboratory/robert/test/encryption/huffman/HuffmanEncoderTest.java
+++ src/test/java/ro/ulbsibiu/ccsd/laboratory/robert/test/encryption/huffman/HuffmanEncoderTest.java
@@ -1,10 +1,12 @@
 package ro.ulbsibiu.ccsd.laboratory.robert.test.encryption.huffman;
 
 import org.junit.jupiter.api.Test;
+import ro.ulbsibiu.ccsd.laboratory.robert.bitio.BitWriter;
 import ro.ulbsibiu.ccsd.laboratory.robert.encoding.huffmanstatic.HuffmanStaticEncoder;
 import ro.ulbsibiu.ccsd.laboratory.robert.encoding.huffmanstatic.Symbol;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
@@ -13,10 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HuffmanEncoderTest {
     @Test
-    void computeDictionary() throws IOException, NoSuchFieldException, IllegalAccessException {
-        byte[] bytes = new byte[]{'A', 'B', 'C', 'D', 'E', 'C', 'D', 'E', 'C', 'D'};
-        InputStream inputStream = new ByteArrayInputStream(bytes);
-        HuffmanStaticEncoder huffmanEncoder = new HuffmanStaticEncoder(null);
+    void computeDictionaryEncodeWriteFlush() throws IOException, NoSuchFieldException, IllegalAccessException {
+        byte[] inputBytes = new byte[]{'A', 'B', 'C', 'D', 'E', 'C', 'D', 'E', 'C', 'D'};
+        InputStream inputStream = new ByteArrayInputStream(inputBytes);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        HuffmanStaticEncoder huffmanEncoder = new HuffmanStaticEncoder(new BitWriter(outputStream));
 
         huffmanEncoder.computeHeader(inputStream);
         huffmanEncoder.computeDictionary();
@@ -35,5 +38,14 @@ public class HuffmanEncoderTest {
         assertEquals(2, dictionary['D'].getSizeInBits());
         assertEquals(0b00, dictionary['E'].getCode());
         assertEquals(2, dictionary['E'].getSizeInBits());
+
+        inputStream = new ByteArrayInputStream(inputBytes);
+        huffmanEncoder.encodeAndWrite(inputStream, outputStream);
+        huffmanEncoder.flush();
+
+        byte[] outputBytes = outputStream.toByteArray();
+        assertEquals(0b10011010, 0xFF & outputBytes[0]);
+        assertEquals(0b11100011, 0xFF & outputBytes[1]);
+        assertEquals(0b00111000, 0xFF & outputBytes[2]);
     }
 }
